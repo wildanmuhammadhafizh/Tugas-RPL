b9ebb36cf2f95ee852b607dfc8e32f7640720afb
Wildan Muhammad Hafizh <wmh2206@gmail.com>
2016-10-01 12:16:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   echo "10114554 - Ocky Marthatiyanda"
     echo "10114128 - Wildan Muhammad Hafizh"
       echo "10114141 - NAdicky Luthfi Haridhi"
-        echo "10114129" - Dimas Praja"    
-    //ha[pus
+        //cobaaaaa
+        //ha[pus
     //tambah
     
