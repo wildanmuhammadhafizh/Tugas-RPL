937aa8c93a0aa1fdca9067084c0a6dceb03ca6dc
Dimas Praja Suwardi <DimasPraja@users.noreply.github.com>
2016-10-01 12:14:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
   echo "10114554 - Ocky Marthatiyanda"
     echo "10114128 - Wildan Muhammad Hafizh"
       echo "10114141 - NAdicky Luthfi Haridhi"
-        //ha[pus
+        echo "10114129" - Dimas Praja"    
+    //ha[pus
     //tambah
     
